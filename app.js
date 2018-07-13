ee8905fc57e2538e75a4d7812b53dcc7983e0219
hcarneiro <hcarneiro@weboo.biz>
2018-07-13 23:36:08 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -8,4 +8,12 @@ express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/cool', (req, res) => res.send(cool()))
   .get('/', (req, res) => res.send('Hello World\n'))
+  .get('/times', (req, res) => {
+    let result = ''
+    const times = process.env.TIMES || 5
+    for (i = 0; i < times; i++) {
+      result += i + ' '
+    }
+    res.send(result)
+  })
   .listen(PORT, HOSTNAME, () => console.log(`Listening on http://${HOSTNAME}:${PORT}`))
