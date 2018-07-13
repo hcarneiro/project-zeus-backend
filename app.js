99cb77087141e294e4d2c27ba8ecb4a6c4e10ae7
hcarneiro <hcarneiro@weboo.biz>
2018-07-13 23:52:06 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const HOSTNAME = process.env.HOSTNAME || 'localhost'
 const { Pool } = require('pg');
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
-  ssl: true
+  ssl: false
 });
 
 express()
