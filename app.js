877ab78fae7ec146c31120ba66c32f52090e1a9b
hcarneiro <hcarneiro@weboo.biz>
2018-07-13 23:20:59 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -1,14 +1,11 @@
-const http = require('http');
+const cool = require('cool-ascii-faces')
+const express = require('express')
+const path = require('path')
+const PORT = process.env.PORT || 5000
+const HOSTNAME = process.env.HOSTNAME || 'localhost'
 
-const hostname = 'localhost';
-const port = 3535;
-
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
-});
-
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+express()
+  .use(express.static(path.join(__dirname, 'public')))
+  .get('/cool', (req, res) => res.send(cool()))
+  .get('/', (req, res) => res.send('Hello World\n'))
+  .listen(PORT, HOSTNAME, () => console.log(`Listening on http://${HOSTNAME}:${PORT}`))
