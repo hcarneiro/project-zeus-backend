d16dc44907ef624316e88db8094630a7fcb71411
hcarneiro <hcarneiro@weboo.biz>
2018-07-13 23:39:43 +0100

    Updated app.js

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const path = require('path')
 const PORT = process.env.PORT || 5000
 const HOSTNAME = process.env.HOSTNAME || 'localhost'
 
+const { Pool } = require('pg');
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: true
+});
+
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .get('/cool', (req, res) => res.send(cool()))
@@ -16,4 +22,15 @@ express()
     }
     res.send(result)
   })
-  .listen(PORT, HOSTNAME, () => console.log(`Listening on http://${HOSTNAME}:${PORT}`))
+  .get('/db', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result = await client.query('SELECT * FROM test_table');
+      res.render('pages/db', result);
+      client.release();
+    } catch (err) {
+      console.error(err);
+      res.send("Error " + err);
+    }
+  })
+  .listen(PORT, HOSTNAME, () => console.log(`Listening on http://${HOSTNAME}:${PORT}`));
