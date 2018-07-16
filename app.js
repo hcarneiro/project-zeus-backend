e0ae84404d20730b7a2991362f883394eb3cc91e
hcarneiro <hcarneiro@weboo.biz>
2018-07-16 14:32:50 +0100

    wip - app.js update with sequelize

--- app.js
+++ app.js
@@ -3,12 +3,32 @@ const express = require('express')
 const path = require('path')
 const PORT = process.env.PORT || 5000
 const HOSTNAME = process.env.HOST || 'localhost'
+const DATABASE_URL = process.env.DATABASE_URL || 'postgres://drcvvsvulvxlbr:eb482de988c4c2db3ff7145eb099146387dc372dece17a5173c56b26899b4ab9@ec2-23-23-93-115.compute-1.amazonaws.com:5432/d17lv613j57ujr'
 
-const { Pool } = require('pg');
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: false
-});
+const Sequelize = require('sequelize')
+console.log('HOST', HOSTNAME)
+console.log('Database URL', DATABASE_URL)
+const db = new Sequelize(DATABASE_URL, {
+  dialect: 'postgres',
+  dialectOptions: {
+    ssl: true
+  },
+  pool: {
+    max: 25,
+    min: 0,
+    idle: 10000
+  }
+})
+
+db
+  .authenticate()
+  .then(() => {
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    console.error('Unable to connect to the database:', err);
+  })
+
 const app = express()
 
 app
@@ -28,6 +48,7 @@ app
     try {
       const client = await pool.connect()
       const result = await client.query('SELECT * FROM test_table');
+      console.log(result)
       res.send(result);
       client.release();
     } catch (err) {
