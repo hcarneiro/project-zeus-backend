07270e995a6489c91a91a8c5327144fafd9ac194
hcarneiro <hcarneiro@weboo.biz>
2018-07-20 00:46:49 +0100

    updated default status page

--- app.js
+++ app.js
@@ -1,25 +1,25 @@
-const cool = require('cool-ascii-faces');
 const express = require('express');
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 const HOSTNAME = process.env.HOST || 'localhost';
+const config = require('./libs/config');
 const appPackage = require('./package');
 const app = express();
 const User = require('./models/user');
 
 // Routing
 app
-  .get('/cool', (req, res) => res.send(cool()))
-  .get('/times', (req, res) => {
-    let result = ''
-    const times = process.env.TIMES || 5
-    for (i = 0; i < times; i++) {
-      result += i + ' '
-    }
-    res.send(result)
-  })
-  .get('/', (req, res) => {
+  .use(cors())
+  .get('/users', (req, res) => {
     User.findAll().then(users => {
       res.send(users)
+    })
+  })
+  .get('/', (req, res) => {
+    res.send({
+      status: 'ok',
+      environment: config.env,
+      version: appPackage.version
     })
   })
   .listen(PORT, HOSTNAME, () => console.log(`Listening on http://${HOSTNAME}:${PORT}`));
