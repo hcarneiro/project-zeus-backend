6372dc7dc6b90dcaacc50bba025c7fd2abeb7d86
hcarneiro <hcarneiro@weboo.biz>
2018-07-20 00:46:34 +0100

    updated databse config

--- libs/database.js
+++ libs/database.js
@@ -1,13 +1,14 @@
 const Sequelize = require('sequelize');
-const config = require('./config');
-const DATABASE_URL = process.env.DATABASE_URL || 'postgres://drcvvsvulvxlbr:eb482de988c4c2db3ff7145eb099146387dc372dece17a5173c56b26899b4ab9@ec2-23-23-93-115.compute-1.amazonaws.com:5432/d17lv613j57ujr';
+const env = (process.env.NODE_ENV || 'local').toLowerCase();
+const envConfig = require(`../config/${env}.json`);
+const DATABASE_URL = process.env.DATABASE_URL || envConfig.database_url;
 
 const database = {};
 
 const db = new Sequelize(DATABASE_URL, {
   dialect: 'postgres',
   dialectOptions: {
-    ssl: true
+    ssl: process.env.DATABASE_URL ? true : false
   },
   pool: {
     max: 25,
