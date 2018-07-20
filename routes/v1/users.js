19335ac01fd3cb4abd7a7190f8dbe81a199ef568
hcarneiro <hcarneiro@weboo.biz>
2018-07-20 16:01:01 +0100

    refactor refactor users route

--- routes/v1/users.js
+++ routes/v1/users.js
@@ -3,22 +3,29 @@ const router = express.Router();
 const Users = require('../../models/users');
 
 router.get('/', async (req, res) => {
-  Users.findAll().then(users => {
+  const users = await Users.findAll();
+
+  if (users) {
     res.send(users);
-  });
+  } else {
+    const notFound = {
+      message: `No users found`
+    };
+    res.send(notFound);
+  }
 });
 
 router.get('/:id', async (req, res) => {
-  Users.findById(req.params.id).then(user => {
-    if (user) {
-      res.send(user);
-    } else {
-      const notFound = {
-        message: `No user found with ID ${req.params.id}`
-      };
-      res.send(notFound);
-    }
-  });
+  const user = await Users.findById(req.params.id);
+
+  if (user) {
+    res.send(user);
+  } else {
+    const notFound = {
+      message: `No user found with ID ${req.params.id}`
+    };
+    res.send(notFound);
+  }
 });
 
 module.exports = router;
