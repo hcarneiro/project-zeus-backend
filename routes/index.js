78adb5cb57dbcdfa214409340a8d98f0707aff17
hcarneiro <hcarneiro@weboo.biz>
2018-07-31 21:42:25 +0100

    Socket.io integrated

--- routes/index.js
+++ routes/index.js
@@ -2,7 +2,6 @@ const express = require('express');
 const router = express.Router();
 const config = require('../libs/config');
 const appPackage = require('../package');
-const server = require('../server');
 
 router.get('/', function getSystemInfo(req, res) {
   res.render('test');
--- server.js
+++ server.js
@@ -8,23 +8,22 @@ app.set('port', config.port);
 const server = http.createServer(app);
 const io = socketIO(server);
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
-  socket.on('disconnect', () => console.log('Client disconnected'));
-});
+server.start = function () {
+  io.on('connection', function(socket) {
+    console.log('Client connected');
 
-io.on('time', (timeString) => {
-  console.log(timeString);
-});
+    socket.on('disconnect', function() {
+      console.log('Client disconnected');
+    });
+  });
 
-setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
-
-server.start = function (next) {
-  server.listen(config.port, config.interface || undefined);
+  server.listen(config.port, config.interface || undefined, function() {
+    console.log('[HTTP] API listening on host ' + config.host);
+  });
 };
 
 server.on('listening', function () {
-  console.log('[HTTP] API listening on host ' + config.host);
+  //console.log('[HTTP] API listening on host ' + config.host);
 });
 
 server.on('error', function (error) {
