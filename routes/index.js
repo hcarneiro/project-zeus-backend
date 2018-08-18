d0705aba28a830230e82efd06dee92c37ec7c761
hcarneiro <hcarneiro@weboo.biz>
2018-08-18 23:44:52 +0100

    Added user info front page of API

--- routes/index.js
+++ routes/index.js
@@ -4,10 +4,18 @@ const config = require('../libs/config');
 const appPackage = require('../package');
 
 router.get('/', function getSystemInfo(req, res) {
+  let user;
+
+  if (req.user) {
+    user = req.user.get({ plain: true });
+    user.auth_token = req.auth_token;
+  }
+
   res.send({
     status: 'ok',
     environment: config.env,
-    version: appPackage.version
+    version: appPackage.version,
+    user
   });
 });
 
